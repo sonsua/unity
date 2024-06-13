ea8a5643e8dcf86ff275f7ded9280b4dc8daecec
sonsua <suason@naver.com>
2024-06-13 21:46:05 +0900

    2024.06.13 싱글톤

---
+++ Game/Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+public static T Instance
+    {
+        get { return instance; }
+        set { instance = value; }
+    }
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = (T)FindObjectOfType(typeof(T));
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
